ead236ac96d563134ca634c119eafd0845ba249f
Alper Goldenberg <alper.goldenberg@gmail.com>
2024-05-14 16:55:06 +0300

    Added event model validations and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
