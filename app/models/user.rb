7c6a1927f2ea44541ce22851c0b82aeb5336a754
Koni Borowiak <borkoni123@yahoo.co.uk>
2024-05-14 11:32:43 +0100

    check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :first_name, :last_name, presence: true
+  validates :email, :password, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
