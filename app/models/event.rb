0bb3508303e6bf196dd3b220712ee3a0863da8e7
Alper Goldenberg <alper.goldenberg@gmail.com>
2024-05-15 19:26:28 +0300

    pg search bar

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search_by_details,
+    against: [:name, :description, :keywords, :location],
+    using: {
+      tsearch: { prefix: true }
+    }
+
   belongs_to :user
   has_many_attached :photos
 end
