e39c21d7c40eacf8ee8134f69f02f4ea28c1dd51
katarzynabaginska <katarzyna.baginska11@icloud.com>
2024-07-08 10:38:17 +0100

    new

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,7 @@ Rails.application.routes.draw do
   get 'events/show'
   devise_for :users
   root to: "events#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+ 
   resources :events do
     resources :bookings
     resources :chatrooms, only: :show
